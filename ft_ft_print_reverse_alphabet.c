d61a3c3006dca6801451f051f45430fe15114f9e
atlee731 <hellosolvur@gmail.com>
2018-07-25 14:22:26 -0700

    Create ft_ft_print_reverse_alphabet.c

---
+++ ft_ft_print_reverse_alphabet.c
@@ -0,0 +1,12 @@
+void ft_print_reverse_alphabet (void)
+ {
+     char count;
+     
+     count = 'z';
+     
+     while (count >= 'a') {
+        printf("%c",count);
+        count--;
+    }
+    return;
+ }
