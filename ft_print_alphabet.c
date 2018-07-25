22ee794fcd3f23f7b235790b151dd3968f3965f2
atlee731 <hellosolvur@gmail.com>
2018-07-25 14:32:15 -0700

    Create ft_print_alphabet.c

---
+++ ft_print_alphabet.c
@@ -0,0 +1,12 @@
+void ft_print_alphabet (void)
++ {
++     char count;
++     
++     count = 'a';
++     
++     while (count <= 'z') {
++        printf("%c",count);
++        count++;
++    }
++    return;
++ }
